98fa4eb6d1051168479ec858d59639e9de0258ee
Joseph5712 <jmm7gameover@gmail.com>
2024-10-29 13:04:17 -0600

    Episodio 15 - Understanding Database Seeders

--- webserver/sites/14days.isw811.xyz/database/migrations/2024_10_29_173431_create_tags_table.php
+++ webserver/sites/14days.isw811.xyz/database/migrations/2024_10_29_173431_create_tags_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tags');
         Schema::dropIfExists('job_tag');
+        Schema::dropIfExists('tags');
     }
 };
